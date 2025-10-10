2dfded18081b48e2fecdbf2d62bd7ea0a5f93018
Surya-narayan-18 <battulasuryanarayan@gmail.com>
2025-10-10 10:45:01 +0530

    Subarray

---
+++ Subarray.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[3]={1,2,3};
+    for(int i=0;i<3;i++){
+        for(int j=i;j<3;j++){
+            for(int k=i;k<=j;k++)
+            cout<<arr[k]<<" ";
+            cout<<endl;
+        }
+
+    }
+}
