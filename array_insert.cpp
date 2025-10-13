f48c2a4240a9be4956ead45d88bd69ad6cb22513
Surya-narayan-18 <battulasuryanarayan@gmail.com>
2025-10-13 11:29:22 +0530

    Insert Array

---
+++ array_insert.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[5]={1,2,3,4,5};
+    for(int i=4;i>0;i--){
+        arr[i]=arr[i-1];
+    }
+    cin>>arr[0];
+    for(int i=0;i<5;i++)
+    cout<<arr[i]<<" ";
+}
